dc042f1af3d792f628d264b99b07f919a1c07469
Jesse Yowell <jesse.yowell@gmail.com>
2018-12-16 12:52:14 -0500

    Adding state and props values to ToDo app

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -3,12 +3,33 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  
+  this.state = {
+
+      todos: [
+
+        { description: "Walk the cat", isCompleted: true },
+        { description: "Feed the baby", isCompleted: false },
+        { description: "Eat the beans", isCompleted: false }
+
+      ]
+  };
+}
+  
+
+
+
   render() {
     return (
       <div className="App">
         <ul>
-          <ToDo />
-          <ToDo />
+            // Below we are sending the state values to props, to be used in the ToDo component 
+
+            { this.state.todos.map( (todo, index) =>
+              <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+            )}
         </ul> 
       </div>
     );
